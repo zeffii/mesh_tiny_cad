76747da3361e0fc1c790490840ae5fce3be7762f
Dealga McArdle <zeffii@users.noreply.github.com>
2014-03-23 14:08:49 +0100

    pep8

--- V2X/vert2intersection.py
+++ V2X/vert2intersection.py
@@ -1,23 +1,23 @@
 '''
 BEGIN GPL LICENSE BLOCK
- 
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- 
+
 END GPL LICENCE BLOCK
 '''
- 
+
 bl_info = {
     "name": "Vert tools : vert 2 intersection",
     "author": "zeffii",
@@ -28,10 +28,11 @@ bl_info = {
     "wiki_url": "",
     "tracker_url": ""
 }
- 
+
 import bmesh
 import bpy
 from mathutils import geometry
+
 
 def add_vertex_to_intersection():
 
@@ -49,34 +50,38 @@ def add_vertex_to_intersection():
         bm.verts.new(iv)
         bmesh.update_edit_mesh(me)
 
- 
+
 class Vert2Intersection(bpy.types.Operator):
- 
+
     bl_idname = 'mesh.vertintersect'
     bl_label = bl_info['name']
     bl_options = {'REGISTER', 'UNDO'}
- 
+
     @classmethod
     def poll(self, context):
         obj = context.active_object
-        return obj != None and obj.type == 'MESH' and obj.mode == 'EDIT'
- 
+        return obj is not None and obj.type == 'MESH' and obj.mode == 'EDIT'
+
     def execute(self, context):
         add_vertex_to_intersection()
         return {'FINISHED'}
- 
- 
+
+
 def menu_func(self, context):
-    self.layout.operator(Vert2Intersection.bl_idname, text="Place vertex at Intersection")
- 
+    self.layout.operator(
+        Vert2Intersection.bl_idname,
+        text="Place vertex at Intersection")
+
+
 def register():
     bpy.utils.register_module(__name__)
     bpy.types.VIEW3D_MT_edit_mesh_specials.append(menu_func)
- 
+
+
 def unregister():
     bpy.utils.unregister_module(__name__)
     bpy.types.VIEW3D_MT_edit_mesh_specials.remove(menu_func)
- 
- 
+
+
 if __name__ == "__main__":
     register()

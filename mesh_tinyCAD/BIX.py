67b3664de7c4b41eb08091079a0f9b72b93d9e89
Dealga McArdle <zeffii@users.noreply.github.com>
2014-04-03 13:03:29 +0200

    shared index was not the cause of the error.

--- mesh_tinyCAD/BIX.py
+++ mesh_tinyCAD/BIX.py
@@ -43,43 +43,32 @@ def add_line_to_bisection(self):
     dist1 = (v1-v2).length
     dist2 = (v3-v4).length
     bdist = min([dist1, dist2])
+    edge1 = (v1, v2)
+    edge2 = (v3, v4)
 
-    [[j1, j2], [j3, j4]] = [[v.index for v in e.verts] for e in edges]
-    a, b = {j1, j2}, {j3, j4}
-    shared = a & b
+    if not cm.test_coplanar(edge1, edge2):
+        msg = "edges must be coplanar non parallel edges"
+        self.report({"WARNING"}, msg)
+        return
 
-    # definitely 'intersects' because shares vertex
-    if len(shared) == 1:
-        print('shares_vertex!')
-        ix1, ix2 = tuple(a ^ b)
-        pt = bm.verts[shared.pop()].co
-        far1 = bm.verts[ix1].co
-        far2 = bm.verts[ix2].co
-    else:
-        edge1 = (v1, v2)
-        edge2 = (v3, v4)
-        if not cm.test_coplanar(edge1, edge2):
-            msg = "edges must be coplanar non parallel edges"
-            self.report({"WARNING"}, msg)
-            return
-        pt = cm.get_intersection(edge1, edge2)
-        # pick furthest vertex from (projected) intersections
-        far1 = v2 if (v1-pt).length < (v2-pt).length else v1
-        far2 = v4 if (v3-pt).length < (v4-pt).length else v3
-    print('intersection: ', pt)
+    # get pt and pick fartest vertex from (projected) intersections
+    pt = cm.get_intersection(edge1, edge2)
+    far1 = v2 if (v1-pt).length < (v2-pt).length else v1
+    far2 = v4 if (v3-pt).length < (v4-pt).length else v3
+    # print('intersection: ', pt)
 
     dex1 = far1 - pt
     dex2 = far2 - pt
     dex1 = dex1 * (bdist / dex1.length)
     dex2 = dex2 * (bdist / dex2.length)
     pt2 = pt + (dex1).lerp(dex2, 0.5)
-    print('bisector point:', pt2)
+    # print('bisector point:', pt2)
 
     vec1 = bm.verts.new(pt2)
     vec2 = bm.verts.new(pt)
     bm.edges.new((vec1, vec2))
     bmesh.update_edit_mesh(me)
-    print("awesome")
+    # print("done")
 
 
 class LineOnBisection(bpy.types.Operator):
